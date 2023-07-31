3f5c30515f9fbcf02757c579bab012ce11a106f0
薄荷你玩 <bhshare.cn@gmail.com>
2023-07-31 13:28:33 +0800

    update

--- data/代理ip获取/代理ip.py
+++ data/代理ip获取/代理ip.py
@@ -238,11 +238,11 @@ from email.mime.text import MIMEText
 from email.header import Header
 
 # 第三方 SMTP 服务
-mail_host = "smtp.163.com"  # 设置服务器
-mail_user = "yaokui_ltd@163.com"  # 用户名
-mail_pass = "yaokui1997"  # 口令
-sender = 'yaokui_ltd@163.com'
-receivers = ['1361453981@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+mail_host = "smtp.xxx.com"  # 设置服务器
+mail_user = "xxx@xxx.com"  # 用户名
+mail_pass = "xxx"  # 口令
+sender = 'xxx@xxx.com'
+receivers = ['xxx@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
 
 def sendEmail(content):
